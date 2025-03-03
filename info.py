ed4b228906fef1589b1b22e411c34a290e4b212d
milky76 <praveenrps90@gmail.com>
2025-03-03 17:01:15 +0530

    Update info.py

--- info.py
+++ info.py
@@ -113,12 +113,12 @@ USE_CAPTION_FILTER = bool(environ.get('USE_CAPTION_FILTER', True))
 
 # Token Verification Info :
 VERIFY = bool(environ.get('VERIFY', True))
-VERIFY_SHORTLINK_URL = environ.get('VERIFY_SHORTLINK_URL', 'instantlinks.co')
-VERIFY_SHORTLINK_API = environ.get('VERIFY_SHORTLINK_API', 'cf0558d19566501b4d224bf2955a758354e68e40')
+VERIFY_SHORTLINK_URL = environ.get('VERIFY_SHORTLINK_URL', 'shortner.in')
+VERIFY_SHORTLINK_API = environ.get('VERIFY_SHORTLINK_API', 'f3f928a7f5d872112dba67bff6931f98c93957fb')
 VERIFY_TUTORIAL = environ.get('VERIFY_TUTORIAL', 'https://t.me/Open_LinkGuide')
 
 # If You Fill Second Shortner Then Bot Attach Both First And Second Shortner And Use It For Verify.
-VERIFY_SECOND_SHORTNER = bool(environ.get('VERIFY_SECOND_SHORTNER', True))
+VERIFY_SECOND_SHORTNER = bool(environ.get('VERIFY_SECOND_SHORTNER', False))
 # if verify second shortner is True then fill below url and api
 VERIFY_SND_SHORTLINK_URL = environ.get('VERIFY_SND_SHORTLINK_URL', 'shortner.in')
 VERIFY_SND_SHORTLINK_API = environ.get('VERIFY_SND_SHORTLINK_API', 'f3f928a7f5d872112dba67bff6931f98c93957fb')
